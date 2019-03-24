64ee8c93334a34de3376d4aa057cfb25765e5f2b
João Leite <joaopleite26@gmail.com>
2019-03-24 13:30:48 -0300

    Initial Commit

---
+++ Splashify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splashify
+//
+//  Created by João Leite on 24/03/19.
+//  Copyright © 2019 João Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
